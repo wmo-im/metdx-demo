c4867afc4f7083e47d50eb10beccbadf2fff3db8
jameshawkes <jameshawkes@users.noreply.github.com>
2026-01-14 15:45:00 +0000

    Deploying to gh-pages from @ wmo-im/metdx-demo@3905c0771bd3d0b70660cd29da7b0d4a39a3ea9b 🚀

--- metdx-demo-ui/flutter_bootstrap.js
+++ metdx-demo-ui/flutter_bootstrap.js
@@ -11,7 +11,7 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
     serviceWorkerSettings: {
-        serviceWorkerVersion: "1400932325",
+        serviceWorkerVersion: "286281209",
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const appRunner = await engineInitializer.initializeEngine({useColorEmoji: useColorEmoji});
