504915997db64470780f34bf43c0b40b5f0c8532
awarde96 <awarde@tcd.ie>
2026-01-15 14:19:33 +0000

    Add license header

--- metdx-demo-api/plugins/processes/chart_generator.py
+++ metdx-demo-api/plugins/processes/chart_generator.py
@@ -1,3 +1,24 @@
+###############################################################################
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+###############################################################################
+
 from pygeoapi.process.base import BaseProcessor, ProcessorExecuteError
 
 import io
@@ -14,12 +35,8 @@ PROCESS_METADATA = {
     "id": "ChartProcessor",
     "title": {"en": "ChartProcessor"},
     "description": {
-        "en": "An example process that takes a name as input, and echoes "
-        "it back as output. Intended to demonstrate a simple "
-        "process with a single literal input.",
-        "fr": "Un exemple de processus qui prend un nom en entrée et le "
-        "renvoie en sortie. Destiné à démontrer un processus "
-        "simple avec une seule entrée littérale.",
+        "en": "An example process that takes a coverageJSON PointSeries as input, "
+        "and returns a timeseries chart. ",
     },
     "jobControlOptions": ["sync-execute", "async-execute"],
     "keywords": ["chart"],
