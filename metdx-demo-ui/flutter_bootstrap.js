2518f062c8e0a1544793d6a69edd7735d6b8b5c1
tomkralidis <tomkralidis@gmail.com>
2026-01-16 15:56:49 +0000

    Deploying to gh-pages from @ wmo-im/metdx-demo@f32a86b81601bdb30cc5b74c4d1658f8ad94429f 🚀

--- metdx-demo-ui/flutter_bootstrap.js
+++ metdx-demo-ui/flutter_bootstrap.js
@@ -52,7 +52,7 @@ if (flet.noCdn) {
 _flutter.loader.load({
     config: flutterConfig,
     serviceWorkerSettings: {
-        serviceWorkerVersion: "4145884797",
+        serviceWorkerVersion: "1667920659",
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const engine = await engineInitializer.initializeEngine(flutterConfig);
