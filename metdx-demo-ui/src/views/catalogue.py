e9f7b080f7229563652657beec772912c6f23a20
awarde96 <awarde@tcd.ie>
2026-01-28 16:09:34 +0100

    Update pyproject to point to correct root for flet deployment

--- metdx-demo-ui/src/views/catalogue.py
+++ metdx-demo-ui/src/views/catalogue.py
@@ -5,9 +5,16 @@ from dataclasses import field
 import json
 from .record import RecordView
 
-GDC_ENDPOINT = "https://f0d0ef280f8c.ngrok-free.app/collections/wis2-discovery-metadata/items"
+# GDC_ENDPOINT = "https://f0d0ef280f8c.ngrok-free.app/collections/wis2-discovery-metadata/items"
+GDC_ENDPOINT = (
+    "https://wis2-gdc.weather.gc.ca/collections/wis2-discovery-metadata/items"
+)
+
 
 def CatalogueView(page: ft.Page):
+
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.update()
 
     def on_click_record(e, record):
         record_view = RecordView(page, record)
@@ -16,13 +23,14 @@ def CatalogueView(page: ft.Page):
     @ft.control
     class SearchResult(ft.Container):
         record: dict = field(default_factory=dict)
+
         def init(self):
 
             props = self.record.get("properties", {})
             title_text = props.get("title", "No Title")
             desc_text = props.get("description", "No description")
             keywords = props.get("keywords", [])
-            
+
             self.padding = 10
             self.border_radius = 5
             self.border = ft.border.all(1, ft.Colors.GREY)
@@ -34,7 +42,7 @@ def CatalogueView(page: ft.Page):
                 weight=ft.FontWeight.BOLD,
                 size=16,
                 max_lines=1,
-                overflow=ft.TextOverflow.ELLIPSIS
+                overflow=ft.TextOverflow.ELLIPSIS,
             )
 
             # Description
@@ -43,31 +51,24 @@ def CatalogueView(page: ft.Page):
                 color=ft.Colors.GREY_800,
                 size=12,
                 max_lines=1,
-                overflow=ft.TextOverflow.ELLIPSIS
+                overflow=ft.TextOverflow.ELLIPSIS,
             )
 
             # Tags
             tags_row = ft.Row(wrap=True, spacing=5)
-            for tag in keywords[:5]: 
+            for tag in keywords[:5]:
                 tags_row.controls.append(
                     ft.Container(
                         content=ft.Text(tag, size=10, color=ft.Colors.WHITE),
                         bgcolor=ft.Colors.BLUE_GREY_600,
                         padding=ft.padding.symmetric(horizontal=8, vertical=2),
-                        border_radius=10
+                        border_radius=10,
                     )
                 )
-            
+
             self.on_click = lambda e: on_click_record(e, self.record)
 
-            self.content = ft.Column(
-                controls=[
-                    title_control,
-                    desc_control,
-                    tags_row
-                ]
-            )
-
+            self.content = ft.Column(controls=[title_control, desc_control, tags_row])
 
     def on_search(e):
         try:
@@ -78,37 +79,40 @@ def CatalogueView(page: ft.Page):
             results = data.get("features", [])
             search_results.controls.clear()
             search_results.controls.append(
-                ft.Text(f"Found {number_of_results} results for '{search_bar.value}'", weight=ft.FontWeight.BOLD)
+                ft.Text(
+                    f"Found {number_of_results} results for '{search_bar.value}'",
+                    weight=ft.FontWeight.BOLD,
+                )
             )
             for item in results:
                 search_results.controls.append(SearchResult(record=item))
-            search_results.visible = True            
+            search_results.visible = True
             page.update()
 
         except Exception as ex:
-            page.show_dialog(ft.SnackBar(
-                ft.Text(f"Error during search: {ex}"),
-                bgcolor=ft.Colors.RED,
-                action="Dismiss"
-            ))
+            page.show_dialog(
+                ft.SnackBar(
+                    ft.Text(f"Error during search: {ex}"),
+                    bgcolor=ft.Colors.RED,
+                    action="Dismiss",
+                )
+            )
 
-    search_bar = TextField(hint_text="Welcome...", autofocus=True, on_submit=on_search, align=ft.Alignment.CENTER, width=600)
+    search_bar = TextField(
+        hint_text="Welcome...",
+        autofocus=True,
+        on_submit=on_search,
+        align=ft.Alignment.CENTER,
+        width=600,
+    )
 
     search_results = ListView(expand=True, spacing=10, padding=20, visible=False)
 
     layout = ft.Column(
-        controls=[
-            search_bar,
-            search_results
-        ],
-        alignment=ft.MainAxisAlignment.CENTER, 
+        controls=[search_bar, search_results],
+        alignment=ft.MainAxisAlignment.CENTER,
         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-        expand=True
+        expand=True,
     )
 
-    return ft.View(
-        route="/",
-        controls=[
-            layout
-        ]
-    )
+    return ft.View(route="/", controls=[layout])
